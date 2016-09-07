cd3529b8e678663d4019ab84cf8149833a563d56
LucasSouriau <lucas.souriau@gmail.com>
2016-09-07 14:26:36 +0200

    Update GameBoard.py

--- SplendorCode/src/game/GameBoard.py
+++ SplendorCode/src/game/GameBoard.py
@@ -1,9 +1,52 @@
-from src.element.Card import Card
-from src.game.Display import Display
-
+import Tools
 
 class GameBoard:
-    display = Display()
-    deck = {Card(),Card()}
-    deck1 = {Card(), Card()}
-    deck2 = {Card(), Card()}
+
+    def __init__(self):
+        self.types = []
+        self.hiddenTiles = []
+        self.displayedTiles = []
+        self.deck = []
+        self.displayedCards = []
+
+    def addType(self, type):
+        self.types.append(type)
+
+    def delTypes(self, type):
+        self.types.remove(type)
+        del type
+
+    def countTypes(self):
+        return len(self.types)
+
+
+
+    def addHiddenTile(self, tile):
+        self.hiddenTiles.append(tile)
+
+    def delHiddenTile(self, tile):
+        self.hiddenTiles.remove(tile)
+
+
+
+    def addDisplayedTile(self, tile):
+        self.displayedTiles.append(tile)
+
+    def delDisplayedTile(self, tile):
+        self.displayedTiles.remove(tile)
+
+
+
+    def addToDeck(self, card):
+        self.deck.append(card)
+
+    def delToDeck(self, card):
+        self.deck.remove(card)
+
+
+
+    def addDisplayedCard(self, card):
+        self.displayedCards.append(card)
+
+    def delDisplayedCard(self, card):
+        self.displayedCards.remove(card)
