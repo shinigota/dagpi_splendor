90620b531bdac0b3df1051605332af0be39cb2c5
LucasSouriau <lucas.souriau@gmail.com>
2016-09-07 14:27:10 +0200

    Update Player.py

--- SplendorCode/src/player/Player.py
+++ SplendorCode/src/player/Player.py
@@ -1,3 +1,46 @@
+import Tools
+import Game
+
 class Player:
-    nickname = ""
-    position = 0
+
+    def __init__(self):
+        self.purchasedCards = []
+        self.reservedCards = []
+        self.ownedTiles = []
+        self.bank = []
+        self.position = None
+        self.nickname = None
+
+        for type in Game.GameBoard.getTypes():
+            self.bank.append(self.initBank(type))
+
+    def initBank(self, type):
+        tkStack = Tools.TokenStack()
+        tkStack.setType(type)
+        tkStack.setNbToken(0)
+        return tkStack
+
+    def addPurchasedCard(self, card):
+        self.purchasedCards.append(card)
+
+    def delPurchasedCard(self, card):
+        self.purchasedCards.remove(card)
+        del card
+
+
+
+    def addReservedCard(self, card):
+        self.reservedCards.append(card)
+
+    def delReservedCard(self, card):
+        self.reservedCards.remove(card)
+        del card
+
+
+
+    def addOwnedTile(self, tile):
+        self.ownedTiles.append(tile)
+
+    def delOwnedTile(self, tile):
+        self.ownedTiles.remove(tile)
+        del tile
