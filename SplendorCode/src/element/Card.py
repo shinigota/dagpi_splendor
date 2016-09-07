addeabd89fb3584ffcc937c9e29d2b5658e7cc0a
LucasSouriau <lucas.souriau@gmail.com>
2016-09-07 14:24:25 +0200

    Update Card.py

--- SplendorCode/src/element/Card.py
+++ SplendorCode/src/element/Card.py
@@ -1,2 +1,18 @@
 class Card:
-    points = 0
+
+    def __init__(self):
+        self.points = None
+        self.incomeGem = None
+        self.purchaseGems =[]
+
+
+    def setIncomeGem(self, tokenStack):
+        self.incomeGem = tokenStack
+
+
+    def addPurchaseGems(self, tokenStack):
+        self.purchaseGems.append(tokenStack)
+
+    def delPurchaseGems(self, tokenStack):
+        self.purchaseGems.remove(tokenStack)
+        del tokenStack
