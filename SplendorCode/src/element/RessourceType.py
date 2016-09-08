b895c424de926b64fa06e68d9132c1015363ea27
Benjamin BARBE <ben.barbe10@gmail.com>
2016-09-08 19:10:23 +0200

    Updated class diagram, added init communication diagram Added xlsx document containing the main game's functions Updated RessourceType

--- SplendorCode/src/element/RessourceType.py
+++ SplendorCode/src/element/RessourceType.py
@@ -1,5 +1,12 @@
-class Type:
-    name = None
+class RessourceType:
+    ressource_type = None
 
     def __init__(self):
-        self.name = None
+        self.ressource_type = dict()
+
+    def add_ressource(self, name, color):
+        self.ressource_type[name] = color
+
+    def get_ressource_color(self, name):
+        return self.ressource_type[name]
+
