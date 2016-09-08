9ad891d7f6cb95d02961434a066f4b04cb914825
damjacques <djacques@IUT.BX1>
2016-09-08 10:33:08 +0200

    Display update

--- SplendorCode/src/game/Display.py
+++ SplendorCode/src/game/Display.py
@@ -4,16 +4,35 @@ from src.game.GameRules import GameRules
 
 
 class Display:
+    """zerghj."""
+    window = None
+    text = "test"
+    isshow = True;
+    w, h = None, None
+
     def __init__(self):
-        self.app = Tk()
-        self.createwindow()
+        """dfgh."""
+        self.window = Tk()
+        self.createwindow()  # fghj
 
     def createwindow(self):
-        self.app.title(GameRules.gameName)
-        w, h = self.app.winfo_screenwidth(), self.app.winfo_screenheight()
-        self.app.overrideredirect(1)
-        self.app.geometry("%dx%d+0+0" % (w, h))
-        self.app.mainloop()
+        self.window.title(GameRules.gameName)
+        # self.w, self.h = self.window.winfo_screenwidth(), self.window.winfo_screenheight()
+        self.w, self.h = 1900, 1000
+        self.window.overrideredirect(0)
+        self.window.geometry("%dx%d+0+0" % (self.w, self.h))
+
+    def displaytile(self, position, tile):
+        canvas = Canvas(self.window, width=100, height=100, background='#c18b01')
+        txt = canvas.create_text(10, 10, text=0, fill="black")
+        txt = canvas.create_text(50, 10, text="R", fill="red")
+        canvas.place(x=250 + 120 * (position - 1), y=120, anchor=SE)
 
 
-Display()
+GameRules()
+display = Display()
+display.displaytile(1, None)
+display.displaytile(2, None)
+display.displaytile(3, None)
+display.displaytile(4, None)
+display.window.mainloop()
--- SplendorCode/src/game/GameRules.py
+++ SplendorCode/src/game/GameRules.py
@@ -20,18 +20,18 @@ class GameRules:
     nbTilePerTurn = 0
     nbMinGemStack = 0
 
-    def __init__(self, gameboard):
-        gameboard.rien = 0
+    def __init__(self):
         tree = ET.parse("../res/splendor_res.xml")
         root = tree.getroot()
         print()
         # Single parameter xml
         for sp in root.findall('single_parameters'):
-            self.gameName = sp.find('game_name').text
-            self.nbLvlCard = sp.find('number_levels_cards').text
-            self.nbMaxResCard = sp.find('number_max_reserved_cards').text
-            self.nbPointsTile = sp.find('number_prestige_points_noble_tiles').text
-            self.nbPointsEnd = sp.find('number_prestige_points_end_game').text
+            GameRules.gameName = sp.find('game_name').text
+            print(self.gameName)
+            GameRules.nbLvlCard = sp.find('number_levels_cards').text
+            GameRules.nbMaxResCard = sp.find('number_max_reserved_cards').text
+            GameRules.nbPointsTile = sp.find('number_prestige_points_noble_tiles').text
+            GameRules.nbPointsEnd = sp.find('number_prestige_points_end_game').text
 
         # Token xml
         for token in root.findall(".//token"):
@@ -42,25 +42,25 @@ class GameRules:
 
         # Game setup xml
         for gs in root.findall('game_setup'):
-            self.nbCardReveal = gs.find('number_cards_to_reveal').text
-            self.nbTilesnbP = gs.find('number_noble_tiles_more_number_players').text
+            GameRules.nbCardReveal = gs.find('number_cards_to_reveal').text
+            GameRules.nbTilesnbP = gs.find('number_noble_tiles_more_number_players').text
             ngtiganp = './/number_gem_tokens_in_game_according_number_players'
             for ngtigan in root.findall(ngtiganp):
-                self.nbGemFor2 = ngtigan.find('number_gem_tokens_in_game_2_players').text
-                self.nbGemFor3 = ngtigan.find('number_gem_tokens_in_game_3_players').text
-                self.nbGemFor4 = ngtigan.find('number_gem_tokens_in_game_4_players').text
-            self.nbGold = gs.find('number_gold_tokens_in_game').text
+                GameRules.nbGemFor2 = ngtigan.find('number_gem_tokens_in_game_2_players').text
+                GameRules.nbGemFor3 = ngtigan.find('number_gem_tokens_in_game_3_players').text
+                GameRules.nbGemFor4 = ngtigan.find('number_gem_tokens_in_game_4_players').text
+            GameRules.nbGold = gs.find('number_gold_tokens_in_game').text
 
         # Turn parameters xml
         for tp in root.findall('turn_parameters'):
-            self.nbGemDiff = tp.find('number_gem_tokens_different_colors').text
-            self.nbGemSame = tp.find('number_gem_tokens_same_color').text
+            GameRules.nbGemDiff = tp.find('number_gem_tokens_different_colors').text
+            GameRules.nbGemSame = tp.find('number_gem_tokens_same_color').text
             nmgtsisc = 'number_min_gem_tokens_stack_if_same_color'
-            self.nbMinGemStack = tp.find(nmgtsisc).text
+            GameRules.nbMinGemStack = tp.find(nmgtsisc).text
             nmgticr = 'number_max_gold_tokens_if_card_reserved'
-            self.nbGoldTake = tp.find(nmgticr).text
-            self.nbTokenEndTurn = tp.find('number_max_tokens_end_turn').text
-            self.nbTilePerTurn = tp.find('number_max_noble_tile_turn').text
+            GameRules.nbGoldTake = tp.find(nmgticr).text
+            GameRules.nbTokenEndTurn = tp.find('number_max_tokens_end_turn').text
+            GameRules.nbTilePerTurn = tp.find('number_max_noble_tile_turn').text
 
 
         # Noble tiles xml
