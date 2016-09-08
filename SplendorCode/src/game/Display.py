31ddac4f93df73de6ca0794ecf9a5d652973fef2
damjacques <djacques@IUT.BX1>
2016-09-08 15:05:44 +0200

    Add Maquette and update Display

--- SplendorCode/src/game/Display.py
+++ SplendorCode/src/game/Display.py
@@ -13,26 +13,40 @@ class Display:
     def __init__(self):
         """dfgh."""
         self.window = Tk()
-        self.createwindow()  # fghj
+        self.create_window()  # fghj
 
-    def createwindow(self):
+    def create_window(self):
         self.window.title(GameRules.game_name)
         # self.w, self.h = self.window.winfo_screenwidth(), self.window.winfo_screenheight()
         self.w, self.h = 1900, 1000
         self.window.overrideredirect(0)
         self.window.geometry("%dx%d+0+0" % (self.w, self.h))
 
-    def displaytile(self, position, tile):
+    def display_tile(self, position, tile):
         canvas = Canvas(self.window, width=100, height=100, background='#c18b01')
         txt = canvas.create_text(10, 10, text=0, fill="black")
         txt = canvas.create_text(50, 10, text="R", fill="red")
         canvas.place(x=250 + 120 * (position - 1), y=120, anchor=SE)
 
+    def display_card(self, position, niveau,card):
+        if niveau == 1:
+            color = '#0483f9'
+        canvas = Canvas(self.window, width=100, height=120, background=color)
+        txt = canvas.create_text(10, 10, text=0, fill="black")
+        txt = canvas.create_text(50, 10, text="R", fill="white")
+        txt = canvas.create_rectangle(50, 10, 60, 20, fill="red")
+        canvas.place(x=250 + 120 * (position - 1), y=300, anchor=SE)
+
 
 GameRules()
 display = Display()
-display.displaytile(1, None)
-display.displaytile(2, None)
-display.displaytile(3, None)
-display.displaytile(4, None)
+display.display_tile(1, None)
+display.display_tile(2, None)
+display.display_tile(3, None)
+display.display_tile(4, None)
+
+display.display_card(1,1, None)
+display.display_card(2,1, None)
+display.display_card(3,1, None)
+display.display_card(4,1, None)
 display.window.mainloop()
