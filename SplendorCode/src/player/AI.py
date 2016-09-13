a5d15c6c3771accb257e00dd18e3ef2023fdc005
unknown <sbonnan@info-thrall.iut.bx1>
2016-09-13 17:31:34 +0200

    AI Change

--- SplendorCode/src/player/AI.py
+++ SplendorCode/src/player/AI.py
@@ -11,12 +11,8 @@ class AI(Player):
     game_state = None
     ressource = None
 
-    def __init__(self, name, position, difficulty, gb, gr, rt, gs):
+    def __init__(self, name, position, difficulty):
         Player.__init__(self, name, position)
-        self.game_board = gb
-        self.game_rules = gr
-        self.ressource = rt
-        self.game_state = gs
         self.dificulty = difficulty
         self.position = position
 
@@ -31,7 +27,7 @@ class AI(Player):
 
             if action_ia == "two":
                 dict_t2 = {}
-                for k, v in self.game_board.bank.items():
+                for k, v in GameBoard.bank.items():
                     if v >= self.game_board.nb_min_gem_stack:
                         dict_t2[k] = v
                 if len(dict_t2) == 0:
