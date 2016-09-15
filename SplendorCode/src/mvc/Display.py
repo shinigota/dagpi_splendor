41987cafe76cb0ee8b0d8d859009d007449a6e70
Benjamin BARBE <ben.barbe10@gmail.com>
2016-09-15 13:05:20 +0200

    fix

--- SplendorCode/src/mvc/Display.py
+++ SplendorCode/src/mvc/Display.py
@@ -2,6 +2,7 @@ from tkinter import *
 
 from src.element.Card import Card
 from src.element.ResourceType import ResourceType
+from src.game.GameState import GameState
 from src.mvc.EventType import EventType
 from src.mvc.GameBoard import GameBoard
 from src.mvc.GameRules import GameRules
@@ -288,7 +289,13 @@ class Display:
         canvas.create_text(30, 30, text=nb, fill=color)
         canvas.place(x=x, y=y)
 
+    def display_text_help(self, text):
+        canvas = Canvas(self.window, width=1920, height=70)
+        canvas.create_text(100, 30, text=text)
+        canvas.place(x=0, y=0)
+
     def popup_select_card_action(self, isreserved, ispurchase, card):
+        # GameState.toggle_modal(True)
         self.popup = Toplevel(height=250, width=280)
         self.popup.protocol("WM_DELETE_WINDOW", self.on_exit)
         Label(self.popup, text="Sélectionnez votre action :", height=1,
@@ -308,13 +315,13 @@ class Display:
                             width=60, background="grey")
             canvas.create_text(30, 10, text="Acheter", fill="black")
             canvas.bind("<Button-1>", lambda event,
-                                             p=self.popup,
                                              e=EventType.POPUP_PURCHASE,
                                              c=card:
-            self.click_on_popup(p, e, c))
+            self.click_on_popup(e, c))
             canvas.place(x=160, y=200)
 
     def popup_select_tile_action(self, tiles):
+        # GameState.toggle_modal(True)
         self.popup = Toplevel(height=170, width=565)
         self.popup.protocol("WM_DELETE_WINDOW", self.on_exit)
         Label(self.popup, text="Sélectionnez votre Noble:", height=1,
@@ -326,14 +333,16 @@ class Display:
             x += 110
 
     def popup_txt(self, txt):
+        # GameState.toggle_modal(True)
         self.popup = Toplevel(height=50, width=280)
         self.popup.protocol("WM_DELETE_WINDOW", self.on_exit)
         Label(self.popup, text=txt, height=1,
               width=30).place(x=40, y=10)
 
     def click_on_popup(self, event, objet):
-        self.game_rules.event(event, objet)
         self.popup.destroy()
+        # GameState.toggle_modal(False)
+        self.game_rules.event(event, objet)
 
     def on_exit(self):
         self.game_rules.event(EventType.CLOSE_POPUP, None)
@@ -569,7 +578,6 @@ class Display:
         self.display_cards()
         self.display_tiles()
         self.display_players()
-        self.popup_select_tile_action(self.game_board.displayed_tiles)
 
 
 display = Display()
